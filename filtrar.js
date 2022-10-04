c7033d2a01645b3a83242db6cab10586528e78ac
Smz1n <samuel.gomesv2@gmail.com>
2022-10-04 15:23:16 -0300

    alterando algumas coisas na tabela

---
+++ filtrar.js
@@ -0,0 +1,29 @@
+function filtrar() {
+    //valor que o usuario digitou
+    let expressao = input_busca.value.toLowerCase();
+
+    //Pegando todas as linhas da tabela
+    let linhas = tabela_contatos.getElementsByTagName('tr');
+
+    for (let posicao in linhas) {
+        if (isNaN(posicao) ) {
+            continue;
+        }
+
+        //numero de colunas para cada item tipo: item, quantidade,e telefone
+
+        let coluna1 = linhas[posicao].children[1].innerText.toLowerCase()
+        let coluna2 = linhas[posicao].children[2].innerText.toLowerCase()
+        let coluna3 = linhas[posicao].children[3].innerText.toLowerCase()
+
+        let colunas = coluna1 + coluna2 + coluna3;
+        // let linha = linhas[posicao].innerText.toLowerCase();
+
+        if( colunas.includes(expressao)) {
+            linhas[posicao].style.display = '';
+        }else {
+            linhas[posicao].style.display = 'none'
+        }
+    }
+
+}
